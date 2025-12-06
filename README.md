97e47b498f47bd4be833f333411891be653b3abf
Aya Salama <asalama0204@gmail.com>
2025-12-05 23:40:19 -0500

    mf_struct

--- README.md
+++ README.md
@@ -19,6 +19,13 @@ python run.py [-v] [example.esql]
 * ```example.esql``` is an optional argument, if it is provided,
 input will be read from the file. Otherwise, you are asked to provide valid esql input. To terminate input, simply type "done".
 
+* If no file is provided as argument, you will get the following output when run: 
+```Choose an option:
+1. ESQL
+2. Phi Operator
+```
+If you choose 1, type the ESQL query and type "done" when you're done. This is equivalent to putting the query in a ```.esql``` file. If you press 2, you will be asked to provide the 6 differen arguments to Phi.
+
 # References
 
 The lexer and parser were inspired by the examples at https://www.dabeaz.com/ply/ply.html#ply_nn9
--- run.py
+++ run.py
@@ -3,56 +3,123 @@ from lex import lexer
 from parse import parser
 from classes.phi import Phi
 
+def mf_struct(phi):
+    result = "mf_struct = {}\n"
+    result += f"mf_struct['{phi.v[0]}'] = []\n"
+    for f in phi.f_vect:
+        result += f"mf_struct['{f}'] = 0\n"
+    return result
+
 def main():
     esql = ""
-    verbose = False 
+    verbose = False
+    opt = 'Phi'
+    phi = Phi()
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], ":v", ["help", "output="])
+        opts, args = getopt.getopt(sys.argv[1:], ":v", ["help"])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
         sys.exit(2)
     verbose = False
-    for o, a in opts:
+    for o, _ in opts:
         if o == "-v":
             verbose = True
+        if o == "-help":
+            print("usage: python run.py [-v] [-help] [example.esql]")
+            sys.exit()
 
     if args:
         file = args[0]
         with open(file, 'r') as f:
             esql = f.read()
+            opt = 'ESQL'
     else: # take input
-        print("ESQL Code (type done to execute):")
         while True:
-            line = input()
-            if line.strip().lower() == "done":
+            print("Choose an option:")
+            option = input("1. ESQL\n2. Phi Operator\n")
+
+            if option == '1':
+                opt = 'ESQL'
+                print("Type 'done' to exit.")
+                while True:
+                    line = input()
+                    if line.strip().lower() == "done":
+                        break
+                    esql += line + "\n"
                 break
-            esql += line + "\n"
+            
+            elif option == '2':
+                """
+                1. s - projected columns / expressions
+                2. n - number of grouping variables
+                3. V - grouping attributes: {self.v}
+                4. F-VECT - vector of aggregate functions: {self.f_vect}
+                5. PRED-LIST - list of predicates for grouping var's: {self.pred_list}
+                6. HAVING: {self.having}"""
 
-    if verbose:
-        print("=======LEXING=======")
-        lexer.input(esql)
-        # Tokenize
-        while True:
-            tok = lexer.token()
-            if not tok: 
-                break      # No more input
-            print(tok)
+                s = input('1. s - projected columns / expressions (separated list by commas): ')
+                if s != 'None':
+                    s = s.strip().split(',')
+                    phi.s = s
 
-    if verbose:
-        print("=======PARSING=======")
-    parsed_esql = parser.parse(esql)
+                n = input('2. n - number of grouping variables (int): ')
+                n = int(n)
+                if n > 0:
+                    phi.n = n
+
+                v = input('3. v - grouping attributes (separated list by commas): ')
+                if v != 'None':
+                    v = v.strip().split(',')
+                    phi.v = v
+
+                f_vect = input('4. F-VECT - vector of aggregate functions (separated list by commas): ')
+                if f_vect != 'None':
+                    f_vect = f_vect.strip().split(',')
+                    phi.f_vect = f_vect
+
+                pred_list = input("5. PRED-LIST - list of predicates for grouping var's (separated list by commas): " )
+                if pred_list != 'None':
+                    pred_list = pred_list.strip().split(',')
+                    phi.pred_list = pred_list
+
+                having = input('6. HAVING: ')
+                if having != 'None':
+                    having = having.strip().split(',')
+                    phi.having = having
+                
+                break
+            else:
+                print("Invalid option, try again.")
+
+    if opt == 'ESQL':
+        if verbose:
+            print("=======LEXING=======")
+            lexer.input(esql)
+            # Tokenize
+            while True:
+                tok = lexer.token()
+                if not tok: 
+                    break      # No more input
+                print(tok)
+
+        if verbose:
+            print("=======PARSING=======")
+        parsed_esql = parser.parse(esql)
+        
+        if verbose:
+            print("ESQL Object:")
+            print(parsed_esql)
+            phi.convert(parsed_esql)
+    
+    mf_struct_str = mf_struct(phi)
     
     if verbose:
-        print("ESQL Object:")
-        print(parsed_esql)
-    
-    phi = Phi()
-    phi.convert(parsed_esql)
-    if verbose:
-        print("Phi Operator:")
+        print("=======Phi Operator=======")
         print(phi)
+        print("=======MF Struct=======")
+        print(mf_struct_str)
     
 if __name__ == "__main__":
     main()
